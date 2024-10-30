8fd3c5b168f83f06e803fab4b9ca17ac2eb433d1
Joseph <joseph.aladas@outlook.com>
2024-10-29 20:15:58 -0400

    booking class

---
+++ code/Booking.java
@@ -0,0 +1,26 @@
+package code;
+public class Booking {
+    private Lesson lesson;
+    private Client client;
+
+    public Booking(){}
+        
+    public Booking(Lesson lesson, Client client) {
+        this.lesson = lesson;
+        this.client = client;
+    }
+    public Lesson getLesson() {
+        return lesson;
+    }
+    public void setLesson(Lesson lesson) {
+        this.lesson = lesson;
+    }
+    public Client getClient() {
+        return client;
+    }
+    public void setClient(Client client) {
+        this.client = client;
+    }
+    
+    
+}
